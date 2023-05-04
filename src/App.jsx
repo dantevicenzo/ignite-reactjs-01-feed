5f972d9e807208b8d9becbf103bfde37ef8cc731
Dante Vicenzo <dantevcz@gmail.com>
2023-05-04 08:26:13 -0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { Header } from './components/Header'
 import './global.css'
 
 function App() {
 
   return (
     <>
-      <h1>
-        Hello World
-      </h1>
+      
+      <Header />
+      
     </>
   )
 }
