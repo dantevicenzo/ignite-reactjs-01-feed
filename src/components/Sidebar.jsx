6173f0eab6a63babdecd2843226fa06acba8e6f0
Dante Vicenzo <dantevcz@gmail.com>
2023-05-04 11:35:56 -0300

    Sidebar style adjustments

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,12 +4,16 @@ import { PencilLine } from "@phosphor-icons/react";
 export function Sidebar(){
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://images.unsplash.com/photo-1605092676920-8ac5ae40c7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1605092676920-8ac5ae40c7c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+            />
 
             <div className={styles.userInfo}>
-                
-                <img className={styles.avatar} src='https://github.com/dantevicenzo.png' />
-                
+                <img 
+                    className={styles.avatar} 
+                    src='https://github.com/dantevicenzo.png' 
+                />
                 <strong>Dante Vicenzo</strong>
                 <span>React Developer</span>
             </div>
